e1952deea62d06321554f98cb99babb117d55d2d
Nicolas AMBROISE <nicolas.ambroise@ctie.etat.lu>
2023-12-18 15:33:27 +0100

    test modal 5

--- a11y/script.js
+++ a11y/script.js
@@ -1,27 +1,41 @@
-// Add CSS file
+// Add JS & CSS files for custom dialog modal 
 let cssModal = document.getElementById('injected-css');
 if (cssModal === null) {
   cssModal = document.createElement('link');
   cssModal.id = 'injected-css';
   cssModal.rel = 'stylesheet';
+  cssModal.href = 'https://nicolasambroise.github.io/a11y/js-modal.css?v=' + Date.now();
   document.getElementsByTagName('head')[0].appendChild(cssModal);
 }
-cssModal.href = 'https://nicolasambroise.github.io/a11y/js-modal.css?v=' + Date.now();
 
-var jsModal = document.createElement('script');
-jsModal.src = 'https://nicolasambroise.github.io/a11y/js-modal.js?v=' + Date.now();
-document.getElementsByTagName('head')[0].appendChild(jsModal);
+let jsModal = document.getElementById('injected-js');
+if (jsModal === null) {
+  jsModal = document.createElement('script');
+  jsModal.id = 'injected-js';
+  jsModal.src = 'https://nicolasambroise.github.io/a11y/js-modal.js?v=' + Date.now();
+  document.getElementsByTagName('head')[0].appendChild(jsModal);
+}
+
+// Init result message
+let result_crit = "<p>test1</p>";
+let result_nc = "<p>test2</p>";
+let result_nth = "<p>test3</p>";
+let result_dev = "<p>test4</p>";
 
 
-var result_crit = "<p>test1</p>";
-var result_nc = "<p>test2</p>";
-var result_nth = "<p>test3</p>";
-var result_dev = "<p>test4</p>";
 
+//  --- Script here ---
+
+
+
+
+
+// Create the dialog Modal
 let NIAmodalA11Y = document.createElement('div');
 NIAmodalA11Y.setAttribute("id", "NIAmodalA11Y");
 NIAmodalA11Y.innerHTML = '<h1>A11Y Review</h1><h2>Points critiques</h2>'+result_crit+'<h2>Points non-conforme</h2>'+result_nc+'<h2>Nice-to-have</h2>'+result_nth+'<h2>Problèmes dev</h2>'+result_dev+'<hr><h2>W3C</h2><p>Todo</p><h2>WAVE</h2><p>Todo</p><h2>Lighthouse</h2><p>Todo</p>';
 document.body.appendChild(NIAmodalA11Y);
 
-//const NIAmodalA11Y = document.querySelector("#NIAmodalA11Y");
-modal.open(NIAmodalA11Y, { allowDrag: true });
+setTimeout(() => {
+  modal.open(NIAmodalA11Y, { allowDrag: true })
+}, 100);
