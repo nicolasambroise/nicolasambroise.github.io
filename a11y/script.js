33a3d1391f02308b3dd53e3b86603c0f9c0788de
Nicolas AMBROISE <nicolas.ambroise@ctie.etat.lu>
2024-01-05 15:32:38 +0100

    add multiple tests 2

--- a11y/script.js
+++ a11y/script.js
@@ -1011,7 +1011,7 @@ o S’assurer que les titres sont dans le bon ordre*/
 	}
 	
 	// D. Presentation attribut
-	const nia10d_nodes = document.querySelectorAll('[align], [alink], [background], [bgcolor], [border], [cellpadding], [cellspacing], [char], [charoff], [clear], [color], [compact], [frameborder], [hspace], [link], [marginheight], [marginwidth], [text], [valign], [vlink], [vspace], [size]:not(select), [width]:not(img):not(object):not(embed):not(canvas):not(svg),[height]:not(img):not(object):not(embed):not(canvas):not(svg)'); 
+	const nia10d_nodes = document.querySelectorAll('[align], [alink], [background], [bgcolor], [border], [cellpadding], [cellspacing], [char], [charoff], [clear], [color], [compact], [frameborder], [hspace], [link], [marginheight], [marginwidth], [text], [valign], [vlink], [vspace], [size]:not(select), *:not(symbol) > *:not(g) > [width]:not(img):not(object):not(embed):not(canvas):not(svg),*:not(symbol) > *:not(g) > [height]:not(img):not(object):not(embed):not(canvas):not(svg)'); 
 	if(nia10d_nodes && nia10d_nodes.length > 0 && isItemsVisible(nia10d_nodes)){
 	  result_nc += "<li><a href='#' data-destination='nia10d' class='result-focus'>10-D</a> : Présence d'attributs HTML servant à la présentation de l'information [<a href='https://accessibilite.public.lu/fr/rgaa4.1.2/criteres.html#test-10-1-2' target='_blank'>RGAA 10.1.2</a>]</li>";
 	  setItemsOutline(nia10d_nodes,"red","nia10d");
