bfff233cebf792fefd3d14d6539992ef10487c99
Nicolas AMBROISE <nicolas.ambroise@ctie.etat.lu>
2024-05-07 14:37:15 +0200

    split script9

--- a11y/script.js
+++ a11y/script.js
@@ -71,125 +71,125 @@ if(!document.body.classList.contains('panel-injected')){
 	}
 
 	if (document.getElementById('injected-js-part01') === null) {
-	  let jsPart01 = document.createElement('script');
+	  var jsPart01 = document.createElement('script');
 	  jsPart01.id = 'injected-js-part01';
 	  jsPart01.src = 'https://nicolasambroise.github.io/a11y/parts/nia01_config.js?v=' + Date.now();
 	  document.head.appendChild(jsPart01);
 	}
 	
 	if (document.getElementById('injected-js-part02') === null) {
-	  let jsPart02 = document.createElement('script');
+	  var jsPart02 = document.createElement('script');
 	  jsPart02.id = 'injected-js-part02';
 	  jsPart02.src = 'https://nicolasambroise.github.io/a11y/parts/nia02_images.js?v=' + Date.now();
 	  document.head.appendChild(jsPart02);
 	}
 	
 	if (document.getElementById('injected-js-part03') === null) {
-	  let jsPart03 = document.createElement('script');
+	  var jsPart03 = document.createElement('script');
 	  jsPart03.id = 'injected-js-part03';
 	  jsPart03.src = 'https://nicolasambroise.github.io/a11y/parts/nia03_links.js?v=' + Date.now();
 	  document.head.appendChild(jsPart03);
 	}
 	
 	if (document.getElementById('injected-js-part04') === null) {
-	  let jsPart04 = document.createElement('script');
+	  var jsPart04 = document.createElement('script');
 	  jsPart04.id = 'injected-js-part04';
 	  jsPart04.src = 'https://nicolasambroise.github.io/a11y/parts/nia04_form.js?v=' + Date.now();
 	  document.head.appendChild(jsPart04);
 	}
 	
 	if (document.getElementById('injected-js-part05') === null) {
-	  let jsPart05 = document.createElement('script');
+	  var jsPart05 = document.createElement('script');
 	  jsPart05.id = 'injected-js-part05';
 	  jsPart05.src = 'https://nicolasambroise.github.io/a11y/parts/nia05_obligatoire.js?v=' + Date.now();
 	  document.head.appendChild(jsPart05);
 	}
 	
 	if (document.getElementById('injected-js-part06') === null) {
-	  let jsPart06 = document.createElement('script');
+	  var jsPart06 = document.createElement('script');
 	  jsPart06.id = 'injected-js-part06';
 	  jsPart06.src = 'https://nicolasambroise.github.io/a11y/parts/nia06_structure.js?v=' + Date.now();
 	  document.head.appendChild(jsPart06);
 	}
 	
 	if (document.getElementById('injected-js-part07') === null) {
-	  let jsPart07 = document.createElement('script');
+	  var jsPart07 = document.createElement('script');
 	  jsPart07.id = 'injected-js-part07';
 	  jsPart07.src = 'https://nicolasambroise.github.io/a11y/parts/nia07_title.js?v=' + Date.now();
 	  document.head.appendChild(jsPart07);
 	}
 	
 	if (document.getElementById('injected-js-part08') === null) {
-	  let jsPart08 = document.createElement('script');
+	  var jsPart08 = document.createElement('script');
 	  jsPart08.id = 'injected-js-part08';
 	  jsPart08.src = 'https://nicolasambroise.github.io/a11y/parts/nia08_table.js?v=' + Date.now();
 	  document.head.appendChild(jsPart08);
 	}
 	
 	if (document.getElementById('injected-js-part09') === null) {
-	  let jsPart09 = document.createElement('script');
+	  var jsPart09 = document.createElement('script');
 	  jsPart09.id = 'injected-js-part09';
 	  jsPart09.src = 'https://nicolasambroise.github.io/a11y/parts/nia09_nav.js?v=' + Date.now();
 	  document.head.appendChild(jsPart09);
 	}
 	
 	if (document.getElementById('injected-js-part10') === null) {
-	  let jsPart10 = document.createElement('script');
+	  var jsPart10 = document.createElement('script');
 	  jsPart10.id = 'injected-js-part10';
 	  jsPart10.src = 'https://nicolasambroise.github.io/a11y/parts/nia10_oldtag.js?v=' + Date.now();
 	  document.head.appendChild(jsPart10);
 	}
 	
 	if (document.getElementById('injected-js-part11') === null) {
-	  let jsPart11 = document.createElement('script');
+	  var jsPart11 = document.createElement('script');
 	  jsPart11.id = 'injected-js-part11';
 	  jsPart11.src = 'https://nicolasambroise.github.io/a11y/parts/nia11_lang.js?v=' + Date.now();
 	  document.head.appendChild(jsPart11);
 	}
 	
 	if (document.getElementById('injected-js-part12') === null) {
-	  let jsPart12 = document.createElement('script');
+	  var jsPart12 = document.createElement('script');
 	  jsPart12.id = 'injected-js-part12';
 	  jsPart12.src = 'https://nicolasambroise.github.io/a11y/parts/nia12_button.js?v=' + Date.now();
 	  document.head.appendChild(jsPart12);
 	}
 	
 	if (document.getElementById('injected-js-part13') === null) {
-	  let jsPart13 = document.createElement('script');
+	  var jsPart13 = document.createElement('script');
 	  jsPart13.id = 'injected-js-part13';
 	  jsPart13.src = 'https://nicolasambroise.github.io/a11y/parts/nia13_lottie.js?v=' + Date.now();
 	  document.head.appendChild(jsPart13);
 	}
 	
 	if (document.getElementById('injected-js-part14') === null) {
-	  let jsPart14 = document.createElement('script');
+	  var jsPart14 = document.createElement('script');
 	  jsPart14.id = 'injected-js-part14';
 	  jsPart14.src = 'https://nicolasambroise.github.io/a11y/parts/nia14_colors.js?v=' + Date.now();
 	  document.head.appendChild(jsPart14);
 	}
 	
 	if (document.getElementById('injected-js-part15') === null) {
-	  let jsPart15 = document.createElement('script');
+	  var jsPart15 = document.createElement('script');
 	  jsPart15.id = 'injected-js-part15';
 	  jsPart15.src = 'https://nicolasambroise.github.io/a11y/parts/nia15_secu.js?v=' + Date.now();
 	  document.head.appendChild(jsPart15);
 	}
 
-	jsPart01.addEventListener('load', () => {check_part_01()})
-	jsPart02.addEventListener('load', () => {check_part_02()})
-	jsPart03.addEventListener('load', () => {check_part_03()})
-	jsPart04.addEventListener('load', () => {check_part_04()})
-	jsPart05.addEventListener('load', () => {check_part_05()})
-	jsPart06.addEventListener('load', () => {check_part_06()})
-	jsPart07.addEventListener('load', () => {check_part_07()})
-	jsPart08.addEventListener('load', () => {check_part_08()})
-	jsPart09.addEventListener('load', () => {check_part_09()})
-	jsPart10.addEventListener('load', () => {check_part_10()})
-	jsPart11.addEventListener('load', () => {check_part_11()})
-	jsPart12.addEventListener('load', () => {check_part_12()})
-	jsPart13.addEventListener('load', () => {check_part_13()})
-	jsPart14.addEventListener('load', () => {check_part_14()})
-	jsPart15.addEventListener('load', () => {check_part_15()})
+	jsPart01.addEventListener('load', () => {if(typeof check_part_01 == "function") check_part_01();})
+	jsPart02.addEventListener('load', () => {if(typeof check_part_02 == "function") check_part_02();})
+	jsPart03.addEventListener('load', () => {if(typeof check_part_03 == "function") check_part_03();})
+	jsPart04.addEventListener('load', () => {if(typeof check_part_04 == "function") check_part_04();})
+	jsPart05.addEventListener('load', () => {if(typeof check_part_05 == "function") check_part_05();})
+	jsPart06.addEventListener('load', () => {if(typeof check_part_06 == "function") check_part_06();})
+	jsPart07.addEventListener('load', () => {if(typeof check_part_07 == "function") check_part_07();})
+	jsPart08.addEventListener('load', () => {if(typeof check_part_08 == "function") check_part_08();})
+	jsPart09.addEventListener('load', () => {if(typeof check_part_09 == "function") check_part_09();})
+	jsPart10.addEventListener('load', () => {if(typeof check_part_10 == "function") check_part_10();})
+	jsPart11.addEventListener('load', () => {if(typeof check_part_11 == "function") check_part_11();})
+	jsPart12.addEventListener('load', () => {if(typeof check_part_12 == "function") check_part_12();})
+	jsPart13.addEventListener('load', () => {if(typeof check_part_13 == "function") check_part_13();})
+	jsPart14.addEventListener('load', () => {if(typeof check_part_14 == "function") check_part_14();})
+	jsPart15.addEventListener('load', () => {if(typeof check_part_15 == "function") check_part_15();})
 	createResultPanel();
 }
 else{
